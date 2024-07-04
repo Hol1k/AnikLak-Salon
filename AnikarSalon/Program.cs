1045fc5ecd506a01ac63247f63cec0900fc9a356
Hol1k <danilugryumov96@gmail.com>
2024-07-04 17:34:06 +0500

    lil fixes on login & signup pages

--- AnikarSalon/Program.cs
+++ AnikarSalon/Program.cs
@@ -11,6 +11,16 @@ app.MapGet("/", async (context) =>
 
 app.MapGet("/login", async (context) =>
 {
+    if (context.Request.Query.Count > 0) context.Response.Redirect("/");
+
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.SendFileAsync("wwwroot/login.html");
+});
+
+app.MapGet("/signup", async (context) =>
+{
+    if (context.Request.Query.Count > 0) context.Response.Redirect("/");
+
     context.Response.ContentType = "text/html; charset=utf-8";
     await context.Response.SendFileAsync("wwwroot/signup.html");
 });
